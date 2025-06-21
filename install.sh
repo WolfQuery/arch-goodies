bdd8c3e8ee4a80a110ca2cc5daea011fd02412bc
WolfQuery <qwertman783@outlook.com>
2025-06-21 23:06:35 +0200

    added i3lock fancy and yay

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "beginning installation"
 
 echo "installing dependencies" 
-sudo pacman -S rofi kitty picom i3blocks eww git unzip fastfetch nano htop vscodium cava feh blueman clipman thunar dunst --needed --noconfirm
+sudo pacman -S rofi kitty picom i3blocks eww git unzip fastfetch nano htop vscodium cava feh blueman clipman thunar dunst make base-devel --needed --noconfirm
 
 echo "creating configuration directories"
 mkdir -p ~/.config/i3/
@@ -38,4 +38,16 @@ cp MapleMonoNL-TTF ~/.local/share/fonts/
 echo "updating font cache"
 fc-cache -fv
 
+echo "installing i3lock-fancy"
+#cd ~/Downloads/
+#git clone https://github.com/meskarune/i3lock-fancy.git
+#cd i3lock-fancy
+#sudo make install
+
+cd ~/Downloads/
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S i3lock-fancy
 
